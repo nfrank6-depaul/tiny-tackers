222cdc9b3bea68530ea637f826af5b15cb4a83e1
Nicole Frank <nfrank6@depaul.edu>
2026-05-19 18:55:32 -0500

    set downwind environment

--- gym_sailing_environments/gym_sail_steps/gym_sail_steps/envs/sailboat_env.py
+++ gym_sailing_environments/gym_sail_steps/gym_sail_steps/envs/sailboat_env.py
@@ -19,17 +19,20 @@ class SailboatEnvUpwind(BoatEnv):
         return super().reset(options, seed)
 
 class SailboatEnvDownwind(BoatEnv):
+    TARGET = (BoatEnv.COURSE_SIZE * 0.5, BoatEnv.COURSE_SIZE * 0.10)
+
     def __init__(self, render_mode=None):
         super().__init__(render_mode)
 
     def reset(self, options=None, seed=None):
         self.boat = SailBoat(
             x=self.COURSE_SIZE * (0.5 + np.random.uniform(-0.2, 0.2)),
-            y=self.COURSE_SIZE * 0.10,
-            heading=self.np_random.random() * np.pi * 2,
+            y=self.COURSE_SIZE * 0.90,
+            heading=-np.pi / 2 + np.random.uniform(-0.75, 0.75),
             heading_dot=np.random.uniform(-0.03, 0.03),
-            speed=np.random.uniform(-1, 0.5),
+            speed=np.random.uniform(0, 0.5),
         )
+
         return super().reset(options, seed)
     
 class SailboatEnvReach(BoatEnv):
